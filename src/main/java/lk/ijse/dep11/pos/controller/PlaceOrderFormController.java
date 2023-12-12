34f8df34572fe4f5006cc49eca72ce3823ee921b
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-13 02:43:28 +0530

    Add getLastOrderId()

--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
@@ -154,7 +154,7 @@ public class PlaceOrderFormController {
         Optional<BigDecimal> orderTotal = tblOrderDetails.getItems().stream()
                 .map(oi -> oi.getTotal())
                 .reduce((prev, cur) -> prev.add(cur));
-        lblTotal.setText("Total: Rs. " + orderTotal.get().setScale(2));
+        lblTotal.setText("Total: Rs. " + orderTotal.orElseGet(()->BigDecimal.ZERO).setScale(2));
 
     }
 
